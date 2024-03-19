5f541f402d88fc1a7be0f14a5e301b6ee0256213
김용빈 <yb@gim-yongbin-ui-MacBookPro.local>
2024-03-19 20:31:29 +0900

    feat : message 삭제 기능 추가

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,18 +9,57 @@ import LogoImg from "../assets/logo.svg?react";
 
 interface Prop {
   disabledLink: boolean,
-  sx: object ,
+  sx: object,
 }
 
-export default function Logo({ disabledLink = false, sx } : Prop ) {
-  // const theme = useTheme();
-  // const PRIMARY_LIGHT = theme.palette.primary.light;
-  // const PRIMARY_MAIN = theme.palette.primary.main;
-  // const PRIMARY_DARK = theme.palette.primary.dark;
+export default function Logo({ disabledLink = false, sx }: Prop) {
 
   const logo = (
-      <LogoImg {...sx} />
+    <LogoImg {...sx} />
   );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   if (disabledLink) {
     return <>{logo}</>;
--- src/components/chat/ChatCard.tsx
+++ src/components/chat/ChatCard.tsx
@@ -3,19 +3,32 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CustomMarkdown from '../common/CommonMarkdown';
 
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
+
 interface Props {
   question: string,
-  answer: string
+  answer: string,
+  messageId: number,
+  handleDeleteButton: (id: number) => void;
 }
 
-const ChatCard = ({ question, answer }: Props) => {
+const ChatCard = ({ question, answer, messageId, handleDeleteButton }: Props) => {
+
+  const onClickDeleteButton = () => {
+    if (!messageId) return;
+    handleDeleteButton(messageId);
+  }
+
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         <Typography variant="h5" component="div">
           <b>{question} </b>
+          <IconButton aria-label="delete" onClick={onClickDeleteButton}>
+            <DeleteIcon />
+          </IconButton>
         </Typography>
-
         <Typography variant="body2">
           <CustomMarkdown text={answer} />
         </Typography>
