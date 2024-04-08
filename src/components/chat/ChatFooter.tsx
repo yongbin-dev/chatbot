99b03de2595d6f150ee4e7614f1d325420693045
김용빈 <yb@gim-yongbin-ui-MacBookPro.local>
2024-04-08 19:46:04 +0900

    style - 채팅 기능 추가

--- src/components/chat/ChatFooter.tsx
+++ src/components/chat/ChatFooter.tsx
@@ -19,27 +19,38 @@ const ChatFooter = ({ handleQuestionButton }: Props) => {
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
     const textDiv = document.getElementById('chatTextArea');
+    const mainDiv = document.getElementById('mainDiv');
 
-    if (!textDiv) {
+    if (!textDiv || !mainDiv) {
       return;
     }
 
     e.preventDefault();
+
+    if (e.ctrlKey && e.key === "Enter") {
+      onClickQuestionButton();
+      return;
+    }
+
     if (e.key === "Enter") {
       if (textDiv.clientHeight <= 300) {
         textDiv.style.height = textDiv.clientHeight + 'px';
+        mainDiv.style.height = mainDiv.clientHeight - 50 + 'px';
+        mainDiv.scrollTop = mainDiv.scrollHeight + 9999;
+        return;
       }
     };
 
     if (e.keyCode == 46 || e.keyCode == 8) {
       if (textDiv.clientHeight >= 180) {
         textDiv.style.height = textDiv.clientHeight - 80 + 'px';
+        mainDiv.style.height = mainDiv.clientHeight + 50 + 'px';
+        mainDiv.scrollTop = mainDiv.scrollHeight + 9999;
+        return;
       }
     }
 
-    if (e.ctrlKey && e.key === "Enter") {
-      onClickQuestionButton();
-    }
+
   };
 
   const onClickQuestionButton = () => {
