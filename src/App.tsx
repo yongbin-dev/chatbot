25b8877c66dfc3f3902f306551048d915e321dd8
ybkim <dydqls5757@gmail.com>
2024-05-17 10:48:10 +0900

    cookie login

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useContext } from "react";
 import Router from "./routes";
 import AuthContext from "./contexts/AuthContext";
 import LoginPage from "./pages/LoginPage";
+import { getCookie } from "./utils/cookieUtil";
 
 function App() {
   const loginContext = useContext(AuthContext);
-  const isLogin = loginContext?.isLogin;
+  const cookieLogin = Boolean(getCookie("login"));
+  const isLogin = loginContext?.isLogin ;
 
-  if (isLogin == false) {
+  if (isLogin == false && cookieLogin != true) {
     return <LoginPage />;
   }
 
