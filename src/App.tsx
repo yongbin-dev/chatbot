edfe74babf4edf380b6badf38d0c6d39063152a8
ybkim <dydqls5757@gmail.com>
2024-05-17 10:53:37 +0900

    fix : import error 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import Router from "./routes";
 import AuthContext from "./contexts/AuthContext";
 import LoginPage from "./pages/LoginPage";
-import { getCookie } from "./utils/cookieUtil";
+import { getCookie } from "./utils/CookieUtil";
 
 function App() {
   const loginContext = useContext(AuthContext);
